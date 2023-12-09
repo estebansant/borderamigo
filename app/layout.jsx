33975e99e0de43a783902cc909756fb384ca0d99
estebansant <estebansant30@gmail.com>
2023-12-09 07:26:30 -0300

    file structure created

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { Header } from "../components/Header/Header";
+
 
 export const metadata = {
   title: "BorderAmigo",
@@ -10,7 +10,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Header />
       <body>{children}</body>
     </html>
   );
