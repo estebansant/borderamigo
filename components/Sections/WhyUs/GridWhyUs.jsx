eba7f265455711ba1afb8504cd1cd6f04cf74511
estebansant <estebansant30@gmail.com>
2023-12-11 23:04:27 -0300

    fixed why us section

--- components/Sections/WhyUs/GridWhyUs.jsx
+++ components/Sections/WhyUs/GridWhyUs.jsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import React from "react";
+import gradient from "@/public/assets/images/gradient.png";
 
 const GridWhyUs = () => {
   return (
@@ -6,45 +8,81 @@ const GridWhyUs = () => {
       {/* I couldn't map through an array and se the variables of the array as tailwind classes to change the background image on hover and using a state to manage all the images on mouse enter and on mouse leave wasn't optimal  */}
       {/* card 1 */}
       <article className="flex-column-center h-auto max-h-[332px] w-auto gap-[26px] rounded-[16px] bg-orange p-[16px]">
-        <div className="duration-[16000ms] h-[225px] w-full max-w-[332px] rounded-[16px] bg-linearGradient bg-mb1 bg-cover bg-center bg-no-repeat transition-all ease-in-out hover:bg-mb1Hover "></div>
+        <div className="duration-[16000ms] flex h-[225px] w-full max-w-[332px] items-end rounded-[16px] bg-mb1 bg-cover bg-center bg-no-repeat transition-all ease-in-out hover:bg-mb1Hover">
+          <Image
+            src={gradient}
+            alt="gradient"
+            className="h-full max-h-[166px] w-full min-w-full"
+          />
+        </div>
         <h6 className="paragraph text-center text-light-gray">
           The only online all-in-one platform for seamless application
         </h6>
       </article>
       {/* card 2 */}
       <article className="flex-column-center h-auto max-h-[332px] w-auto gap-[26px] rounded-[16px] bg-orange p-[16px]">
-        <div className="duration-[16000ms] h-[225px] w-full max-w-[332px] rounded-[16px] bg-linearGradient bg-mb2 bg-cover bg-center bg-no-repeat transition-all ease-in-out hover:bg-mb2Hover "></div>
         <h6 className="paragraph text-center text-light-gray">
           Experienced Visa Specialists
         </h6>
+        <div className="duration-[16000ms] flex h-[225px] w-full max-w-[332px] items-end rounded-[16px] bg-linearGradient bg-mb2 bg-cover bg-center bg-no-repeat transition-all ease-in-out hover:bg-mb2Hover">
+          <Image
+            src={gradient}
+            alt="gradient"
+            className="h-full max-h-[166px] w-full min-w-full"
+          />
+        </div>
       </article>
       {/* card 3 */}
       <article className="flex-column-center h-auto max-h-[332px] w-auto gap-[26px] rounded-[16px] bg-orange p-[16px]">
-        <div className="duration-[16000ms] h-[225px] w-full max-w-[332px] rounded-[16px] bg-linearGradient bg-mb3 bg-cover bg-center bg-no-repeat transition-all ease-in-out hover:bg-mb3Hover "></div>
+        <div className="duration-[16000ms] flex h-[225px] w-full max-w-[332px] items-end rounded-[16px] bg-linearGradient bg-mb3 bg-cover bg-center bg-no-repeat transition-all ease-in-out hover:bg-mb3Hover">
+          <Image
+            src={gradient}
+            alt="gradient"
+            className="h-full max-h-[166px] w-full min-w-full"
+          />
+        </div>
         <h6 className="paragraph text-center text-light-gray">
           AI- Powered Process
         </h6>
       </article>
       {/* card 4 */}
       <article className="flex-column-center h-auto max-h-[332px] w-auto gap-[26px] rounded-[16px] bg-orange p-[16px]">
-        <div className="duration-[16000ms] h-[225px] w-full max-w-[332px] rounded-[16px] bg-linearGradient bg-mb4 bg-cover bg-center bg-no-repeat transition-all ease-in-out hover:bg-mb4Hover "></div>
         <h6 className="paragraph text-center text-light-gray">
           Multilingual consultants
         </h6>
+        <div className="duration-[16000ms] flex h-[225px] w-full max-w-[332px] items-end rounded-[16px] bg-linearGradient bg-mb4 bg-cover bg-center bg-no-repeat transition-all ease-in-out hover:bg-mb4Hover">
+          <Image
+            src={gradient}
+            alt="gradient"
+            className="h-full max-h-[166px] w-full min-w-full"
+          />
+        </div>
       </article>
       {/* card 5 */}
       <article className="flex-column-center h-auto max-h-[332px] w-auto gap-[26px] rounded-[16px] bg-orange p-[16px]">
-        <div className="duration-[16000ms] h-[225px] w-full max-w-[332px] rounded-[16px] bg-linearGradient bg-mb5 bg-cover bg-center bg-no-repeat transition-all ease-in-out hover:bg-mb5Hover "></div>
+        <div className="duration-[16000ms] flex h-[225px] w-full max-w-[332px] items-end rounded-[16px] bg-linearGradient bg-mb5 bg-cover bg-center bg-no-repeat transition-all ease-in-out hover:bg-mb5Hover">
+          <Image
+            src={gradient}
+            alt="gradient"
+            className="h-full max-h-[166px] w-full min-w-full"
+          />
+        </div>
         <h6 className="paragraph text-center text-light-gray">
           Half the price to get your residence
         </h6>
       </article>
       {/* card 6 */}
       <article className="flex-column-center h-auto max-h-[332px] w-auto gap-[26px] rounded-[16px] bg-orange p-[16px]">
-        <div className="duration-[16000ms] h-[225px] w-full max-w-[332px] rounded-[16px] bg-linearGradient bg-mb6 bg-cover bg-center bg-no-repeat transition-all ease-in-out hover:bg-mb6Hover "></div>
         <h6 className="paragraph text-center text-light-gray">
           Highly efficient workflow
         </h6>
+        <div className="duration-[16000ms] flex h-[225px] w-full max-w-[332px] items-end rounded-[16px] bg-linearGradient bg-mb6 bg-cover bg-center bg-no-repeat transition-all ease-in-out hover:bg-mb6Hover">
+          <Image
+            src={gradient}
+            alt="gradient"
+            className="h-full max-h-[166px] w-full min-w-full"
+          />
+        </div>
       </article>
     </div>
   );
