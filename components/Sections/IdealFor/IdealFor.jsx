e3e738a70f86ed75107bf6d41e899008d25840d9
estebansant <estebansant30@gmail.com>
2023-12-12 03:15:57 -0300

    finished ideal for section

--- components/Sections/IdealFor/IdealFor.jsx
+++ components/Sections/IdealFor/IdealFor.jsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
 import React from "react";
+import { IdealCards } from "@/constants/IdealCards";
 
 const IdealFor = () => {
-  return <div>IdealFor</div>;
+  return (
+    <section className="flex-column-center relative h-fit w-full gap-[40px] bg-pink px-[16px] pb-[100px]">
+      <h2 className="h3-bold text-center text-black">Ideal for</h2>
+      <div className="flex-column-center gap-[32px] md:flex-row">
+        {IdealCards.map((card, i) => {
+          return (
+            <article
+              key={i}
+              className="flex-column-start h-auto w-auto max-w-[358px] gap-[36px] rounded-[16px] bg-white p-[20px]"
+            >
+              <Image src={card.image} alt={card.alt} width={316} height={162} />
+              <div className="flex-column-start gap-[16px]">
+                <h4 className="h5-bold max-w-[318px] text-black md:h-[64px]">
+                  {card.title}
+                </h4>
+                <p className="small-paragraph max-w-[318px] text-black">
+                  {card.text}
+                </p>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export { IdealFor };
---
+++ constants/IdealCards.jsx
@@ -0,0 +1,22 @@
+import img from "@/public/assets/images/ideal_image.png";
+
+export const IdealCards = [
+  {
+    image: img,
+    alt: "image alt",
+    title: "Remote workers working for companies",
+    text: "This visa presents extraordinary overseas remote work enablement for staff with location-independent roles. Experience Spain's enviable lifestyle perks with employer support.",
+  },
+  {
+    image: img,
+    alt: "image alt",
+    title: "Freelancers",
+    text: "Spain's nomad-friendly visa gives affordable access for freelancers and self-employed pros to run their solo gigs from Spain's vibrant shores, without breaking the bank.",
+  },
+  {
+    image: img,
+    alt: "image alt",
+    title: "Remote worker with degrees or field of experience",
+    text: "Highly experienced remote workers in technical, business and creative fields will find Spain's lower cost of living and vibrant culture foster both career success and greater life richness.",
+  },
+];
