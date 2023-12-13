c2ffc764907aa5c3268177ed8ecf3300bc2b370e
estebansant <estebansant30@gmail.com>
2023-12-13 09:37:29 -0300

    added favicon and seo metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import Head from "next/head";
 
 export const metadata = {
   title: "Visa Pulse | Get Digital Nomad Visa for Spain Seamlessly",
@@ -9,6 +10,34 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Visa Pulse | Get Digital Nomad Visa for Spain Seamlessly</title>
+        <meta
+          name="description"
+          content="Want Spain's new remote worker visa? DigitalNomadVisa.io helps qualified nomads get approved with complete how-to guides, resources and application services for stress-free Spain residency."
+          key="desc"
+        />
+
+        <meta property="og:title" content="Visa Pulse" />
+        <meta
+          property="og:description"
+          content="Get your Digital Nomad Visa for Spain Seamlessly"
+        />
+
+        <link rel="shortcut icon" href="/public/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/public/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/public/favicon-16x16.png"
+        />
+      </Head>
       <body>{children}</body>
     </html>
   );
